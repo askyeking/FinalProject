06fa492e614f34e356ad2d6b5ad90f1193fdd061
ndobrosa <ndobrosa@gmail.com>
2018-12-02 14:02:54 -0700

    Added Spring security configuration

---
+++ SwagREST/src/main/java/com/skilldistillery/swag/security/securityConfig.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.swag.security;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class securityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	DataSource dataSource;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	        http
+	        .csrf().disable()
+	        .authorizeRequests()
+	        .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()  // For CORS, the preflight request will hit the OPTIONS on the route
+	        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+	        .antMatchers("/login").permitAll()
+	        .antMatchers("/register").permitAll()
+	        .antMatchers("/*").permitAll()
+	        .anyRequest().authenticated()
+	        .and()
+	        .httpBasic();
+
+	        http
+	        .sessionManagement()
+	        .sessionCreationPolicy(SessionCreationPolicy.STATELESS); 
+	  }
+
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		String userQuery = "SELECT email, password, active FROM User WHERE email=?";
+		String authQuery = "SELECT email, role FROM User WHERE email=?";
+		auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery(userQuery)
+				.authoritiesByUsernameQuery(authQuery).passwordEncoder(encoder);
+	}
+}
