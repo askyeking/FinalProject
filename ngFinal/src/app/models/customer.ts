1091451142dad8a637f688b3a24332cf6a77de51
ndobrosa <ndobrosa@gmail.com>
2018-12-03 10:10:40 -0700

    customer and vendor stubbed out in the FE

--- ngFinal/src/app/models/customer.ts
+++ ngFinal/src/app/models/customer.ts
@@ -1,2 +1,18 @@
 export class Customer {
+  id: number;
+  displayName: string;
+  isActive: boolean;
+  avatarURL: string;
+
+  constructor(
+    id: number,
+    isActive: boolean,
+    displayName: string,
+    avatarURL: string
+  ) {
+    this.id = id;
+    this.isActive = isActive;
+    this.displayName = displayName;
+    this.avatarURL = avatarURL;
+  }
 }
