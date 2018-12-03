abf7eda96b41a3dd6ea548631cffc58d43e02924
ndobrosa <ndobrosa@gmail.com>
2018-12-03 09:33:24 -0700

    user can log in, register, and log out in the FE

--- ngFinal/src/app/app-routing.module.ts
+++ ngFinal/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
