098c93d4e253fae91b89bd1ffefb30d42c993725
askyeking <anthony.s.king1992@gmail.com>
2018-12-04 09:11:05 -0700

    got login from navbar working

--- ngFinal/src/app/navigation/navigation.component.ts
+++ ngFinal/src/app/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  loginUser = new User();
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
+
+
+login(user: User) {
+  console.log('in here');
+  console.log(user);
+
+  this.authService.login(user.email, user.password).subscribe(
+    data => {
+      // this.todo.selected = user;
+      this.loginUser = new User();
+      // this.openTodos();
+    },
+    err => {
+      console.error('Observer got an error' + err);
+    }
+  );
+  // this.router.navigateByUrl('todo');
+}
+
+
 
   ngOnInit() {
+
   }
 
 }
