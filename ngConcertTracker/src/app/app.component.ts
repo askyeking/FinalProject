622772d18c9d22bf6a8f978fbe6d2e9d63811888
askyeking <anthony.king.dev@gmail.com>
2024-09-25 14:01:32 -0600

    Stub out Angular app; stub out front-end user auth

---
+++ ngConcertTracker/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'ngConcertTracker';
+
+
+  constructor(
+    private auth: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.tempTestDeleteMeLater(); // DELETE LATER!!!
+  }
+
+  tempTestDeleteMeLater() {
+    this.auth.login('test','test').subscribe({ // change username to match DB
+      next: (data) => {
+        console.log('Logged in:');
+        console.log(data);
+      },
+      error: (fail) => {
+        console.error('Error authenticating:')
+        console.error(fail);
+      }
+    });
+  }
+}
