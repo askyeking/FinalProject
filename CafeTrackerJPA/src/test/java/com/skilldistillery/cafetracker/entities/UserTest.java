70fd15e53cad4f9c18fb267d198d43a72d090bec
askyeking <anthony.king.dev@gmail.com>
2025-06-11 13:42:35 -0600

    stub out database; test stack

--- CafeTrackerJPA/src/test/java/com/skilldistillery/cafetracker/entities/UserTest.java
+++ CafeTrackerJPA/src/test/java/com/skilldistillery/cafetracker/entities/UserTest.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.cafetracker.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -8,27 +9,43 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("CafeTrackerJPA");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_user_basic_mappings() {
+		assertEquals("test", user.getUsername());
+		assertEquals("admin", user.getRole());
+		assertTrue(user.isEnabled());
 	}
 
 }
